1a629f1844291d7e75d1cafff9dbad8c6f6fa526
yakuma01 <yakuma@iu.edu>
2022-06-23 03:17:45 -0400

    Data Wrangler commit
    CSV data handling component removed from Data Fetcher script.
    Handled raw GitHub data using a temporary fix.
    Changed colour of the floor to metallic blue, and colour of the pointer to green when hovering over teleportable floor.

--- CCF3DOrganGallery/Assets/Scripts/CellTypeDataFetcher.cs
+++ CCF3DOrganGallery/Assets/Scripts/CellTypeDataFetcher.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -13,23 +12,28 @@ public class CellTypeDataFetcher : MonoBehaviour
     private CellTypeData _cellTypeData;
     // Reference to TissueBlockData script
     private TissueBlockData _tissueBlockData;
+    string _hid;
     // String field to be accessed to store the data read from GitHub
     public string resultText;
+    // To store the cubes that have been hit
+    private List<GameObject> _hitTissueBlocks;
 
     private void Awake()
     {
         // Passing references of scripts on awakening
         _cellTypeData = gameObject.GetComponent<CellTypeData>();
         _tissueBlockData = gameObject.GetComponent<TissueBlockData>();
+        _hid = _tissueBlockData.HubmapId;
+        //
+        _hitTissueBlocks = new List<GameObject>();
     }
 
     // IEnumerator for Get
     IEnumerator GetCsv()
     {
         // Fetching HubMapID from TissueBlockData script to insert in url 
-        string hid = _tissueBlockData.HubmapId;
         weblink = "https://raw.githubusercontent.com/hubmapconsortium/tissue-bar-graphs/static/csv/Skin_Soumya_et_al__paper/" +
-                  hid + ".csv";
+                  _hid + ".csv";
         // Calls for data from raw-github-link 
         UnityWebRequest www = UnityWebRequest.Get(weblink);
         yield return www.SendWebRequest();
@@ -43,54 +47,49 @@ public class CellTypeDataFetcher : MonoBehaviour
         else
         {
             // Show results as text
-            var x = www.downloadHandler.text;
+            var x = tempFormat(www.downloadHandler.text);
+            
             resultText = x;
+
             // Parsing results
+            
             var data = CsvReader.Read(x);
 
             // Loop to log / show results
-            // for (var i = 0; i < data.Count - 1; i++)
-            // {
-            //     //Test (will remove once the dataset and code is fixed)
-            //     Debug.Log(data[i]["\"cell_type\""]);
-            //     // Debug.Log(data[i]["\"cat\""]);
-            //     // Debug.Log(data[i]["\"sex\""]);
-            //     // Debug.Log(data[i]["\"age\""]);
-
-            //     //Code to log parsed data into the serialized fields for easy access by Visualization script
-            //     _cellTypeData.cell_type[i] = (string)data[i]["\"cell_type\""];
-            //     // _cellTypeData.count[i] =  (int)data[i]["count"];
-            //     // _cellTypeData.percentage[i] = (float)data[i]["\"percentage\""];
-            //     // _cellTypeData.cat[i] = (string)data[i]["\"cat\""];
-            //     // _cellTypeData.sex[i] = (string)data[i]["\"sex\""];
-            //     // _cellTypeData.exp[i] = (string)data[i]["\"exp\""];
-            //     // _cellTypeData.age[i] = (int)data[i]["\"age\""];
-            //     // _cellTypeData.y_pos[i] = (float)data[i]["\"y_pos\""];
-
-            //     // Supplementary string variable holding all the data to show proof of concept temporarily.
-            //     resultText = "Cell Type " + data[i]["\"cell_type\""] + " " +
-            //                  "Cell Count " + data[i]["\"count\""] + " " +
-            //                  "Percentage " + data[i]["\"percentage\""] + " " +
-            //                  "Cat " + data[i]["\"cat\""] + " " +
-            //                  "Sex " + data[i]["\"sex\""] + " " +
-            //                  "Exp " + data[i]["\"exp\""] + " " +
-            //                  "Age " + data[i]["\"age\""] + " " +
-            //                  "Y_Po " + data[i]["\"y_pos\""] + " ";
-            // }
+            for (var i = 0; i < data.Count ; i++)
+            {
+                //Code to log parsed data into the serialized fields for easy access by Visualization script
+                _cellTypeData.cell_type.Add((string)data[i]["\"cell_type\""]);
+                _cellTypeData.count.Add((int)data[i]["\"count\""]);
+                _cellTypeData.percentage.Add((float)data[i]["\"percentage\""]);
+                _cellTypeData.cat.Add((string)data[i]["\"cat\""]);
+                _cellTypeData.sex.Add((string)data[i]["\"sex\""]);
+                _cellTypeData.exp.Add((string)data[i]["\"exp\""]);
+                _cellTypeData.age.Add((int)data[i]["\"age\""]);
+                var yPos = data[i]["\"y_pos\""];
+                if (ReferenceEquals(yPos, ""))
+                {
+                    _cellTypeData.y_pos.Add(0f);
+                }
+                else
+                {
+                    _cellTypeData.y_pos.Add((float)yPos);
+                }
+            }
         }
     }
 
     private void OnEnable()
     {
         // Subscribes events to respective method
-        // TissueBlockSelectActions.OnHover += DisplayHubMapID; // Displays HubMapID when tissue-block is hovered upon
+        TissueBlockSelectActions.OnHover += DisplayHubMapID; // Displays HubMapID when tissue-block is hovered upon
         TissueBlockSelectActions.OnSelected += LogCellTypeInfo; // Logs cell information when tissue-block is selected to CellTypeData script
     }
 
     private void OnDestroy()
     {
         // Unsubscribes events to respective method
-        // TissueBlockSelectActions.OnHover -= DisplayHubMapID;
+        TissueBlockSelectActions.OnHover -= DisplayHubMapID;
         TissueBlockSelectActions.OnSelected -= LogCellTypeInfo;
     }
 
@@ -99,64 +98,30 @@ public class CellTypeDataFetcher : MonoBehaviour
     {
         if (hit.collider.gameObject.Equals(this.gameObject))
         {
-            int startHbmId = weblink.IndexOf("HBM", StringComparison.Ordinal);
-            var hubMapID = weblink.Substring(startHbmId, 15);
-            Debug.Log(hubMapID);
+            // int startHbmId = weblink.IndexOf("HBM", StringComparison.Ordinal);
+            // var hubMapID = weblink.Substring(startHbmId, 15);
+            Debug.Log(_hid);
         }
     }
 
     // Responsible for printing cell type information when tissue-block is selected
     private void LogCellTypeInfo(RaycastHit hit)
     {
-        if (hit.collider.gameObject.Equals(this.gameObject))
+        var currentHitTissueBlock = hit.collider.gameObject;
+        if (!_hitTissueBlocks.Contains(currentHitTissueBlock))
         {
-            StartCoroutine(GetCsv());
+            if (currentHitTissueBlock.Equals(this.gameObject))
+            {
+                StartCoroutine(GetCsv());
+            }
+            _hitTissueBlocks.Add(currentHitTissueBlock);
         }
     }
-
-    // CSVReader to read csv data. Code taken from: https://bravenewmethod.com/2014/09/13/lightweight-csv-reader-for-unity/
-    private class CsvReader
+    
+    // Temporary fix for line break in raw Soumya et al skin data
+    private string tempFormat(string csvText)
     {
-        static string SPLIT_RE = @",(?=(?:[^""]*""[^""]*"")*(?![^""]*""))";
-        static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
-        static readonly char[] TrimChars = { '\"' };
-
-        public static List<Dictionary<string, object>> Read(string file)
-        {
-            var list = new List<Dictionary<string, object>>();
-
-            var lines = Regex.Split(file, LINE_SPLIT_RE);
-
-            if (lines.Length <= 1) return list;
-
-            var header = Regex.Split(lines[0], SPLIT_RE);
-            for (var i = 1; i < lines.Length; i++)
-            {
-                var values = Regex.Split(lines[i], SPLIT_RE);
-                if (values.Length == 0 || values[0] == "") continue;
-
-                var entry = new Dictionary<string, object>();
-                for (var j = 0; j < header.Length && j < values.Length; j++)
-                {
-                    string value = values[j];
-                    value = value.TrimStart(TrimChars).TrimEnd(TrimChars).Replace("\\", "");
-                    object finalValue = value;
-                    if (int.TryParse(value, out var n))
-                    {
-                        finalValue = n;
-                    }
-                    else if (float.TryParse(value, out var f))
-                    {
-                        finalValue = f;
-                    }
-
-                    entry[header[j]] = finalValue;
-                }
-
-                list.Add(entry);
-            }
-
-            return list;
-        }
+        var firstIndex = csvText.IndexOf("/", StringComparison.Ordinal);
+        return csvText.Remove(firstIndex - 1, 1);
     }
 }
