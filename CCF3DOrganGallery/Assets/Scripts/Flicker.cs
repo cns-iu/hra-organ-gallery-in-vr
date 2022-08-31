3f708512a830b2d29e0af12114458aa72db68391
Andreas Bueckle <abueckle@iu.edu>
2022-08-31 10:57:06 -0400

    Remove loading message when scene is built

--- CCF3DOrganGallery/Assets/Scripts/Flicker.cs
+++ CCF3DOrganGallery/Assets/Scripts/Flicker.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Threading.Tasks;
+using TMPro;
 
 public class Flicker : MonoBehaviour
 {
     [SerializeField] private Image image;
     private void OnEnable()
     {
-        SceneBuilder.OnSceneBuilt += () => { image.color = new Color(image.color.r, image.color.g, image.color.b, 0f); Destroy(this); };
+        SceneBuilder.OnSceneBuilt += () =>
+        {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+            Destroy(GetComponentInChildren<TMP_Text>().gameObject);
+            Destroy(this);
+        };
     }
 
     private void Update()
     {
-        image.color = new Color(image.color.r, image.color.g, image.color.b, GetOpacity().Remap(-1,1,0,1));
+        image.color = new Color(image.color.r, image.color.g, image.color.b, GetOpacity().Remap(-1, 1, 0, 1));
     }
 
 
