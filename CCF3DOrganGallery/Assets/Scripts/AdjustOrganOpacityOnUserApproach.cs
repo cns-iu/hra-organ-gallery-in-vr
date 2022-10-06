129cf5d7e20ddcc0b3bf9e664944e8972e473f73
Andreas Bueckle <abueckle@iu.edu>
2022-10-06 13:57:45 -0400

    Adjusted organ opacity on user approach, label direction adjusted only once at teleport

---
+++ CCF3DOrganGallery/Assets/Scripts/AdjustOrganOpacityOnUserApproach.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class AdjustOrganOpacityOnUserApproach : MonoBehaviour
+{
+    [Header("Fade")]
+    [SerializeField] private float maxAlpha = .8f;
+    [SerializeField] private float minAlpha = .2f;
+    [SerializeField] private float fadeDuration = 10f;
+
+    [Header("Collider")]
+    [SerializeField] private float radiusMultiplier = 2f;
+    [SerializeField] private SphereCollider col;
+    public int OpacityOnApproach { get; }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneBuilder.SetOrganOpacity(gameObject, maxAlpha);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        SceneBuilder.SetOrganOpacity(gameObject, minAlpha);
+    }
+
+    public void SetCollider()
+    {
+        SphereCollider col = GetComponent<SphereCollider>();
+        col.radius = col.radius * radiusMultiplier;
+    }
+}
