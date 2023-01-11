aec4091f735794c83b5c0b4e53dc9ec37b302fdd
Andreas Bueckle <abueckle@iu.edu>
2023-01-11 17:38:18 -0500

    URL now built at runtime and editable from inspector

--- CCF3DOrganGallery/Assets/Scripts/Test/CCFAPISPARQLQueryTester.cs
+++ CCF3DOrganGallery/Assets/Scripts/Test/CCFAPISPARQLQueryTester.cs
@@ -1,61 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
-using TMPro;
-using UnityEngine.InputSystem;
-using System.Net;
-using static CellTypeInfoLogger;
 using System.Threading.Tasks;
 using System;
 using UnityEngine.Networking;
-using Unity.VisualScripting;
 
 public class CCFAPISPARQLQueryTester : MonoBehaviour
 {
     [SerializeField] private string baseUrl;
-    [SerializeField] private List<string> iris = new List<string>();
+    [SerializeField] private string iri;
     [SerializeField] private string endpoint;
     [SerializeField] private string format;
 
-    //documentation: http://grlc.io/api/hubmapconsortium/ccf-grlc/ccf/#/default/get_cell_by_location
-    //url: http://grlc.io/api-git/hubmapconsortium/ccf-grlc/subdir/ccf//cell_by_location
-    //?location=http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FUBERON_0002113
-    //&endpoint=https%3A%2F%2Fccf-api.hubmapconsortium.org%2Fv1%2Fsparql
-    //?format=application%2Fjson
-
-    //http://grlc.io/api-git/hubmapconsortium/ccf-grlc/subdir/ccf//cell_by_location?location=http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FUBERON_0002113&endpoint=https%3A%2F%2Fccf-api.hubmapconsortium.org%2Fv1%2Fsparql?format=application%2Fjson
-
-    [SerializeField] private InputActionReference fButtonPressed;
-    [SerializeField] private TMP_Text text;
-
-    private void OnEnable()
-    {
-        fButtonPressed.action.performed += CallAPI;
-        fButtonPressed.action.performed += (InputAction.CallbackContext ctx) => { Debug.Log("called"); };
-    }
-
-    private void OnDisable()
-    {
-        fButtonPressed.action.performed -= CallAPI;
-    }
-
+    //documentation for SPARQL query through grlc: http://grlc.io/api/hubmapconsortium/ccf-grlc/ccf/#/default/get_cell_by_location
+    //url with iri descending colon as query string: http://grlc.io/api-git/hubmapconsortium/ccf-grlc/subdir/ccf//cell_by_location?location=http://purl.obolibrary.org/obo/UBERON_0001158&endpoint=https%3A%2F%2Fccf-api.hubmapconsortium.org%2Fv1%2Fsparql?format=application%2Fjson
 
     async private void Start()
     {
-        string url = baseUrl + iris[0] + endpoint + format;
+        string url = baseUrl + iri + endpoint + format;
         Debug.Log(url);
-        text.text = await Get("http://grlc.io/api-git/hubmapconsortium/ccf-grlc/subdir/ccf//cell_by_location?location=http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FUBERON_0002113&endpoint=https%3A%2F%2Fccf-api.hubmapconsortium.org%2Fv1%2Fsparql?format=application%2Fjson");
+        response = await Get(url);
     }
 
-    async void CallAPI(InputAction.CallbackContext ctx)
-    {
-        Debug.Log("works");
-        string url = baseUrl + iris[0] + "&location=" + endpoint;
-        text.text = await Get(url);
-    }
+    public SPARQLAPIResponse response;
 
-    public async Task<string> Get(string url)
+    public async Task<SPARQLAPIResponse> Get(string url)
     {
         try
         {
@@ -72,14 +41,14 @@ public class CCFAPISPARQLQueryTester : MonoBehaviour
 
             var text = www.downloadHandler.text;
 
-            // _nodeArray = JsonUtility.FromJson<NodeArray>(
-            //     "{ \"pairs\":" +
-            //     text
-            //     + "}"
-            //     );
-            //return _nodeArray;
-            Debug.Log("Response: " + result);
-            return result;
+            response = JsonUtility.FromJson<SPARQLAPIResponse>(
+                "{ \"pairs\":" +
+                text
+                + "}"
+                );
+            return response;
+            //Debug.Log("Response: " + result);
+
         }
         catch (Exception ex)
         {
@@ -97,7 +66,7 @@ public class CCFAPISPARQLQueryTester : MonoBehaviour
     [Serializable]
     public class CellIriLabel
     {
-        public string string_label;
+        public string cell_label;
         public string cell_iri;
     }
 }
