1746c88a8fdc260bc682872d271d66a19f3dc4d7
Devon <descoles@iu.edu>
2023-03-15 15:35:08 -0400

    Retrieving pairs using HashSet DONE
    Scrolly view taken out and retrieving the pairs using a HashSet is implemented. This allows us to get rid of duplicates without having to manually sort through a list.

--- CCF3DOrganGallery/Assets/Scripts/CCFAPISPARQLQuery.cs
+++ CCF3DOrganGallery/Assets/Scripts/CCFAPISPARQLQuery.cs
@@ -20,46 +20,65 @@ public class CCFAPISPARQLQuery : MonoBehaviour
     //documentation for SPARQL query through grlc: http://grlc.io/api/hubmapconsortium/ccf-grlc/ccf/#/default/get_cell_by_location
     //url with iri descending colon as query string: http://grlc.io/api-git/hubmapconsortium/ccf-grlc/subdir/ccf//cell_by_location?location=http://purl.obolibrary.org/obo/UBERON_0001158&endpoint=https%3A%2F%2Fccf-api.hubmapconsortium.org%2Fv1%2Fsparql?format=application%2Fjson
 
+
     public async void CallAPI(SelectEnterEventArgs args)
     {
         // assigns string iri to link from CCFAnnotations element 0
         // adds everything to variable URL and outputs into untiy console
         // outputs into unity console 
         // makes api call with Get(url)
+        string[] iris = args.interactableObject.transform.gameObject.GetComponent<TissueBlockData>().CcfAnnotations;
+        cellLabelSet.Clear();
+        for (int i = 0; i < iris.Length; i++)
+        {
+            string url = baseUrl + iris[i] + endpoint + format;
+            Debug.Log($"now calling: {url}");
+            response = await Get(url);
+            Debug.Log($"received from {url}: {response.pairs[0].cell_label}");
+            cellLabelSet.Add(response.pairs[0].cell_label);
+        }
+
+        Debug.Log($"ran all {iris.Length} API queries");
         
-
-        string iri = args.interactableObject.transform.gameObject.GetComponent<TissueBlockData>().CcfAnnotations[0];
-        string url = baseUrl + iri + endpoint + format;
-        response = await Get(url);
     }
-
-  [SerializeField] private SPARQLAPIResponse response = new SPARQLAPIResponse();
-
-  public string Pairs
+/* TESTING HASHSET BELOW
+    void Awake() {
+        HashSet<string> test = new HashSet<string>();
+        test.Add("label1");
+        test.Add("label1");
+        foreach (var s in test) {
+            Debug.Log(s);
+        }
+    }*/
+    
+    [SerializeField] private SPARQLAPIResponse response = new SPARQLAPIResponse();
+    HashSet<string> cellLabelSet = new HashSet<string>();
+    public string Pairs //pairs method to reference when SetPaneltext is called.
     {
         get
-        {
+        {   
+            //loop through hashset below and return string of labels
 
             StringBuilder sb = new StringBuilder();
-            if (response.pairs.Length > 0)
-            {
-                foreach (var p in response.pairs)
-                {
-                    sb.Append(p.cell_label + ',');
-                }
-            }
-            for (int i = 0; i < sb.Length; i++)
-            {
-                if (sb[i] == '\"')
-                {
-                    sb[i] = ' ';
-                }
 
+            foreach (var label in cellLabelSet)
+            {
+                sb.Append(label);
             }
+
+            sb.Replace("\"\"", ",");
+            sb.Replace("\"", "");
+            int count = 0;
 
             return sb.ToString();
         }
     }
+/*
+    public void clearSet(SelectExitEventargs args)
+    {
+        //use this method to set cellLabelSet
+        args.interactableObject.
+    }*/
 
     public async Task<SPARQLAPIResponse> Get(string url)
     {
