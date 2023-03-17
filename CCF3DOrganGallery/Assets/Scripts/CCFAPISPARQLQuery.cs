ba74d8a9286732ed314b0acbf2c42267f09f7419
Andreas Bueckle <abueckle@iu.edu>
2023-03-17 13:18:40 -0400

    Added overloads for hooking into TissueBlockSelectActions in main scene

--- CCF3DOrganGallery/Assets/Scripts/CCFAPISPARQLQuery.cs
+++ CCF3DOrganGallery/Assets/Scripts/CCFAPISPARQLQuery.cs
@@ -50,19 +50,40 @@ public class CCFAPISPARQLQuery : MonoBehaviour
     [Header("Scene")]
     [SerializeField] private XRRayInteractor _interactor;
     private UnityAction<SelectEnterEventArgs> _selectAction;
+    private UnityAction _onSelected;
 
     //documentation for SPARQL query through grlc: http://grlc.io/api/hubmapconsortium/ccf-grlc/ccf/#/default/get_cell_by_location
     //url with iri descending colon as query string: http://grlc.io/api-git/hubmapconsortium/ccf-grlc/subdir/ccf//cell_by_location?location=http://purl.obolibrary.org/obo/UBERON_0001158&endpoint=https%3A%2F%2Fccf-api.hubmapconsortium.org%2Fv1%2Fsparql?format=application%2Fjson
 
     private void OnEnable()
     {
+        //use this subscription for testing in dev room
         _selectAction += CheckForCellTypes;
 
         _interactor.selectEntered.AddListener(
             _selectAction
             );
+
+        //use this subscription for main scene
+        TissueBlockSelectActions.OnSelected += CheckForCellTypes;
     }
 
+    //overload for main scene
+    public void CheckForCellTypes(RaycastHit hit)
+    {
+        GameObject interactable = hit.collider.gameObject.gameObject;
+
+        if (interactable.GetComponent<TissueBlockData>() == null) return;
+        string[] iris = interactable.GetComponent<TissueBlockData>().CcfAnnotations;
+        _queryResult.triples.Clear();
+        for (int i = 0; i < iris.Length; i++)
+        {
+            List<Cell> result = _apiResponse.triples.Where(n => n.as_iri == iris[i]).ToList();
+            _queryResult.triples.AddRange(result);
+        }
+    }
+
+    //overload for dev room
     public void CheckForCellTypes(SelectEnterEventArgs args)
     {
         GameObject interactable = args.interactableObject.transform.gameObject;
@@ -90,7 +111,8 @@ public class CCFAPISPARQLQuery : MonoBehaviour
         }
 
         _interactor = GetComponent<XRRayInteractor>();
-        await GetAllCellTypes();
+        
+        //await GetAllCellTypes();
     }
 
     public async Task GetAllCellTypes()
