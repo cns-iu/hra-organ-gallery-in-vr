544bd644508a6fbb854870f8edaa577c889649c0
Andreas Bueckle <abueckle@iu.edu>
2023-03-23 17:12:00 -0400

    Revamped UI

--- CCF3DOrganGallery/Assets/Scripts/SetPanelText.cs
+++ CCF3DOrganGallery/Assets/Scripts/SetPanelText.cs
@@ -3,25 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System.Text;
-using static CCFAPISPARQLQuery;
 using System.Linq;
+using UnityEngine.UI;
+using System;
+using Unity.VisualScripting;
+using UnityEngine.Windows;
 
 public class SetPanelText : MonoBehaviour
 {
-    [SerializeField] private TMP_Text _cellTypeText;
+    //[SerializeField] private TMP_Text _cellTypeText;
+    [SerializeField] private List<TMP_Text> _text = new List<TMP_Text>();
+    [SerializeField] private List<GameObject> _cellTypeTexts = new List<GameObject>();
     [SerializeField] private int _cellTypesToShow = 10;
-    private Dictionary<string, int> _dictFrequencies = new Dictionary<string, int>();
     [SerializeField] private List<string> flattened = new List<string>();
+    private Dictionary<string, int> _dictFrequencies = new Dictionary<string, int>();
     private CCFAPISPARQLQuery _query;
 
     private void OnEnable()
     {
+
+        CCFAPISPARQLQuery.OnNewTissueBlockSelected += EnableDisplay;
         CCFAPISPARQLQuery.OnNewTissueBlockSelected += UpdateText;
     }
 
     private void OnDestroy()
     {
+        CCFAPISPARQLQuery.OnNewTissueBlockSelected -= EnableDisplay;
         CCFAPISPARQLQuery.OnNewTissueBlockSelected -= UpdateText;
+    }
+
+    void EnableDisplay(Dictionary<string, HashSet<string>> dict)
+    {
+        if (!_text[2].enabled) return;
+
+        _text[2].enabled = false;
+        Image[] images = GetComponentsInChildren<Image>();
+        for (int i = 0; i < images.Length; i++)
+        {
+            images[i].color = new Color(images[i].color.r, images[i].color.g, images[i].color.b, 1f);
+        }
     }
 
 
@@ -29,27 +49,31 @@ public class SetPanelText : MonoBehaviour
     {
         GetFrequencyOfCellsPerAs(args);
 
-        StringBuilder sb = new StringBuilder();
-        sb.Append($"<b>HuBMAP ID</b>: {CCFAPISPARQLQuery.Instance.HubmapIdOfSelected}\n<b>");
-        sb.Append("\n");
+        //HuBMAP ID
+        _text[0].text = CCFAPISPARQLQuery.Instance.HubmapIdOfSelected == null ? "Not from HuBMAP" : CCFAPISPARQLQuery.Instance.HubmapIdOfSelected;
 
-        sb.Append("<b>Collides with anatomical structure(s)</b>: ");
+        //occurs in how many AS
+        StringBuilder sb = new StringBuilder();
         foreach (var kvp in args)
         {
-            sb.Append(kvp.Key + "\n");
+            string input = kvp.Key.ToString();        
+            string uberonId = input.Substring(input.LastIndexOf("/")+1);
+            sb.Append("\u2022<indent=1em>" + uberonId + "</indent>\n");
         }
+        _text[1].text = sb.ToString();
 
-        sb.Append("\n");
-        sb.Append($"<b>Top-{_cellTypesToShow} expected cell types</b>:\n");
-
+        //top 10 cells
         var sortedDict = from entry in _dictFrequencies orderby entry.Value descending select entry;
         var result = sortedDict.Take(_cellTypesToShow);
 
+        int iterator = 0;
         foreach (var kvp in result)
         {
-            sb.Append($"{kvp.Key} in {kvp.Value} AS\n");
+            TMP_Text[] texts = _cellTypeTexts[iterator].GetComponentsInChildren<TMP_Text>();
+            texts[0].text = kvp.Value.ToString() + "AS";
+            texts[1].text = kvp.Key.ToString().Trim('\"');
+            iterator++;
         }
-        _cellTypeText.text = sb.ToString();
     }
 
     void GetFrequencyOfCellsPerAs(Dictionary<string, HashSet<string>> args)
@@ -63,8 +87,6 @@ public class SetPanelText : MonoBehaviour
             {
                 flattened.Add(item);
             }
-
-
         }
 
         foreach (var cell in flattened)
