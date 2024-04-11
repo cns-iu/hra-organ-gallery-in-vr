2beba8c2f39a3a9b7a07cf16e74247d0b116eb5c
Andreas Bueckle <abueckle@iu.edu>
2024-04-11 15:10:52 -0400

    Added elevator button components to buttons

---
+++ hra-organ-gallery/Assets/Scripts/Interaction/ElevatorManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HRAOrganGallery
+{
+    public class ElevatorManager : MonoBehaviour
+    {
+       
+    }
+}
