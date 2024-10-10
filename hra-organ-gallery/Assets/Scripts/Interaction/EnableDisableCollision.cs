ece8fc0e41447b9a91fdc3c91011a1aba6d279fa
Andreas Bueckle <abueckle@iu.edu>
2024-10-10 18:56:20 -0400

    Fixed bug with raycasting

--- hra-organ-gallery/Assets/Scripts/Interaction/EnableDisableCollision.cs
+++ hra-organ-gallery/Assets/Scripts/Interaction/EnableDisableCollision.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace HRAOrganGallery
 {
     public class EnableDisableCollision : MonoBehaviour
     {
-        [SerializeField] BoxCollider _grabCylinderCollider;
+        [SerializeField] List<Collider> _colliders = new List<Collider>();
         [SerializeField] private bool _isCollidingWithPriority = false;
 
         private void Awake()
@@ -24,9 +25,10 @@ namespace HRAOrganGallery
         private void EnableDisable(bool collides)
         {
             _isCollidingWithPriority = collides;
-            
+
             //ignore collision if we are colliding with a high priority collider, e.g., on the keyboard
-            Physics.IgnoreCollision(_grabCylinderCollider, GetComponent<Collider>(), _isCollidingWithPriority);
+            _colliders.ForEach(c => { Physics.IgnoreCollision(c, GetComponent<Collider>(), _isCollidingWithPriority); });
+            
 
             //old code to turn off entire collider --too brutish
             //_grabCylinderCollider.enabled = !_isCollidingWithPriority;
